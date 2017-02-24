28b838ef784bc3ff48084317ed5a050d240a80e9
dtrt1 <psyfiiend@gmail.com>
2017-02-24 15:56:17 +0300

    Initial commit. Added initial tests.

---
+++ test/core_spec.js
@@ -0,0 +1,24 @@
+/**
+ * Created by DTRT on 24.02.2017.
+ */
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setEntries} from '../src/core';
+
+describe('application logic', () => {
+
+    describe('setEntries', () => {
+
+        it('добавляет записи к состоянию', () => {
+            const state = Map();
+            const entries = List.of('Trainspotting', '28 Days Later');
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        });
+
+    });
+
+});
---
+++ test/test_helper.js
@@ -0,0 +1,6 @@
+/**
+ * Created by DTRT on 24.02.2017.
+ */
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+chai.use(chaiImmutable);
