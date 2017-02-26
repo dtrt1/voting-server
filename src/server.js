53f5b0d7e2ce34a538e2dc076dd05bddb76be1bf
dtrt1 <psyfiiend@gmail.com>
2017-02-26 15:34:13 +0300

    Added connection handlers

--- src/server.js
+++ src/server.js
@@ -3,10 +3,16 @@
  */
 import Server from 'socket.io';
 
-export default function startServer(store) {
+export function startServer(store) {
     const io = new Server().attach(1001);
 
     store.subscribe(
         () => io.emit('state', store.getState().toJS())
     );
+
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+    });
+
 }
